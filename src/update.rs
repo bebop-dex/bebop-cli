16dfccc8d3f2ac702485edf21597fbb0406c3536
Max <max.zub@bebop.xyz>
2026-04-03 12:57:13 +0200

    update fix

--- src/update.rs
+++ src/update.rs
@@ -1,10 +1,12 @@
 use self_update::cargo_crate_version;
 
 pub fn update() {
+    let target = self_update::get_target();
     let status = self_update::backends::github::Update::configure()
         .repo_owner("bebop-dex")
         .repo_name("bebop-cli")
         .bin_name("bebop")
+        .bin_path_in_archive(&format!("bebop-{}/{{{{bin_name}}}}", target))
         .show_download_progress(true)
         .current_version(cargo_crate_version!())
         .build()
